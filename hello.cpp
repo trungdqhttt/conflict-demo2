cb33f84c1e6102cfd248a9a4e0330c48d5e55c74
trungdqhttt <58856461+trungdqhttt@users.noreply.github.com>
2020-03-16 11:44:40 +0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Xin chao thay va cac ban\n";
+    cout << "Xin chao thay va cac ban, minh dang quay video ve conflict tren 2 user\n";
 
     system("pause");
     return 0;
