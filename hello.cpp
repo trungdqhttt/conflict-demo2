ca8d78a7bd5439b1d52b299057883304fc6c0f47
trungdqhttt <trungdq.httt@gmail.com>
2020-03-16 11:45:17 +0700

    changed file

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Xin chao thay va cac ban\n";
+    cout << "Xin chao thay va cac ban, cac ban xem se bi conflict ngay cho nay\n";
 
     system("pause");
     return 0;
